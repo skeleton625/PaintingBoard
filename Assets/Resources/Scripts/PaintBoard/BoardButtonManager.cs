ea70eeb6292b091840d34b31fb343a2b9e3e9dda
skeleton625 <chaos0824@naver.com>
2019-11-04 21:44:01 +0900

    PaintBoard Scene의 ButtonManager 스크립트 수정

---
+++ Assets/Resources/Scripts/PaintBoard/BoardButtonManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardButtonManager : MonoBehaviour
+{
+    private BoardManager GameBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameBoard = GameObject.Find("StaticObject").GetComponent<BoardManager>();
+    }
+
+    public void OnColorButtonClick()
+    {
+        int _selectNum = int.Parse(gameObject.name.Split('_')[1]);
+        GameBoard.SetPixelColor(_selectNum);
+    }
+
+    public void OnIamgeWriteButtonClick()
+    {
+        GameBoard.CreateImageFiles();
+    }
+}
