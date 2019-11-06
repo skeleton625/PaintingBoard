0946b8a042eea2eb1fedb5d3bacdeffbcb758ca2
skeleton625 <chaos0824@naver.com>
2019-11-07 00:37:13 +0900

    MapCreator Scene에서의 Object 생성 함수 수정

--- Assets/Resources/Scripts/MapCreator/MapManager.cs
+++ Assets/Resources/Scripts/MapCreator/MapManager.cs
@@ -43,12 +43,13 @@ public class MapManager : MonoBehaviour
     /* Raycast Hit 정보 */
     private RaycastHit hitInfo;
     private bool isCrafting;
+    private int[,] typeValues;
+    private string[] colorValues;
 
     /* 사용할 오브젝트들을 메모리에 저장해 둠 */
     void Start()
     {
         theTerrain = GetComponent<Terrain>();
-
         /* 오브젝트를 둘 수 있는 블록 오브젝트 초기화 */
         preSelectedBlock = settingBlock;
         selectedObject = settingBlock;
@@ -56,9 +57,11 @@ public class MapManager : MonoBehaviour
 
         /* 맵 데이터 초기화 */
         int terrainSize = ObjectManager.getTerrainSize();
-        theTerrain.terrainData = generateTerrain(theTerrain.terrainData,0, 0,
-            terrainSize, terrainSize, 4);
+        theTerrain.terrainData = generateTerrain(theTerrain.terrainData,0, 0, terrainSize, terrainSize, 4);
+        initiateValues();
         generateSettingBlock();
+        /* 머신 러닝으로 생성한 데이터를 통해 맵 오브젝트를 생성 */
+        readText();
     }
 
     void Update()
@@ -73,6 +76,28 @@ public class MapManager : MonoBehaviour
         selectedType = _type;
         selectedName = _name;
         isObjectSelected = true;
+    }
+
+    private void initiateValues()
+    {
+        typeValues = new int[4, 4];
+        colorValues = new string[6];
+        StreamReader typeValueFile = new StreamReader(@"Datas/PixelTypeValues.csv");
+        StreamReader colorValueFile = new StreamReader(@"Datas/PixelColorValues.csv");
+        for(int i = 0; i < 4; i++)
+        {
+            string[] typeVal = typeValueFile.ReadLine().Split(',');
+            for (int j = 0; j < 4; j++)
+            {
+                typeValues[i, j] = int.Parse(typeVal[j]);
+            }
+                
+        }
+        for(int i = 0; i < 6; i++)
+        {
+            string[] colorVal = colorValueFile.ReadLine().Split(',');
+            colorValues[i] = colorVal[2];
+        }
     }
 
     private void resetMap()
@@ -115,11 +140,11 @@ public class MapManager : MonoBehaviour
             switch (selectedType)
             {
                 case "Tree":
-                    selectedObject = ObjectManager.getTree(selectedName);
+                    selectedObject = ObjectManager.getObject(selectedName);
                     craftingNum = int.Parse(selectedName);
                     break;
                 case "Stone":
-                    selectedObject = ObjectManager.getStone(selectedName);
+                    selectedObject = ObjectManager.getObject(selectedName);
                     craftingNum = int.Parse(selectedName);
                     break;
                 case "Water":
@@ -133,7 +158,6 @@ public class MapManager : MonoBehaviour
     /* 마우스를 통해 원하는 자리에 오브젝트를 생성하는 함수 */
     private void createObjectToPoint()
     {
-        
         if (craftingNum != 0 && !isPanelOn)
         {
             string[] selectedNum = curSelectedBlock.name.Split('_');
@@ -233,52 +257,86 @@ public class MapManager : MonoBehaviour
         }
     }
 
-    public static void readText()
+    private int transObjectNumber(float _inp, string color)
     {
-        if (new FileInfo("map_data_rot.csv").Exists && new FileInfo("map_data_type.csv").Exists)
+        int typeFront = 0;
+        for(int i = 0; i < 6; i++)
+        {
+            if (colorValues[i] == color)
+            {
+                typeFront = i;
+                break;
+            }
+        }
+
+        Debug.Log(typeFront);
+        if (typeFront == 1)
+            return 14;
+        else if (_inp <= 0 || typeFront == 0)
+            return 0;
+        else if (_inp < 0.05)
+            return typeValues[typeFront - 2, 0];
+        else if (_inp < 0.08)
+            return typeValues[typeFront - 2, 1];
+        else if (_inp > 0.08)
+            return typeValues[typeFront - 2, 2];
+        return 0;
+    }
+
+    private void readText()
+    {
+        string preTypePath = @"Datas/CreatedImageText/Image_2.csv";
+        string typePath = @"Datas/CreatedImageData/prediction_type.csv";
+        string rotPath = @"Datas/CreatedImageData/prediction_rot.csv";
+        if (new FileInfo(typePath).Exists && new FileInfo(rotPath).Exists)
         {
             int terrainSize = ObjectManager.getTerrainSize();
-            StreamReader data_type = new StreamReader("map_data_type.csv", false);
-            StreamReader data_rot = new StreamReader("map_data_rot.csv", false);
+            StreamReader data_preType = new StreamReader(preTypePath, false);
+            StreamReader data_type = new StreamReader(typePath, false);
+            StreamReader data_rot = new StreamReader(rotPath, false);
             string[] line_type;
             string[] line_rot;
+            string[] line_preType; 
 
             for (int i = 0; i < terrainSize; i++)
             {
-                GameObject clone = null;
-                line_type = data_type.ReadLine().Replace(" ", "").Split(',');
-                line_rot = data_rot.ReadLine().Replace(" ", "").Split(',');
-                for (int j = 0; j < terrainSize-1; j++)
+                line_preType = data_preType.ReadLine().Split(',');
+                line_type = data_type.ReadLine().Split(',');
+                line_rot = data_rot.ReadLine().Split(',');
+                for (int j = 0; j < terrainSize - 1; j++)
                 {
-                    int types = int.Parse(line_type[j]);
+                    Debug.Log(line_type[j]);
+                    int types = transObjectNumber(float.Parse(line_type[j]), line_preType[j]);
 
-                    if (types == 0 || (i%4 != 0 || j%4 != 0))
+                    if (i%4 != 0 || j%4 != 0)
                         continue;
 
                     int x = i / 4, z = j / 4;
-                    ObjectManager.setSelectedObject(x, z, types);
-
-                    if (types < 11)
-                        clone = Instantiate(ObjectManager.getTree(line_type[j]),
-                                        new Vector3(i+2, 4,j+2),
-                                        Quaternion.EulerAngles(0f, float.Parse(line_rot[j]), 0f));
-                    else if(types < 14)
-                        clone = Instantiate(ObjectManager.getStone(line_type[j]),
-                                        new Vector3(i+2, 4, j+2),
-                                        Quaternion.EulerAngles(0f, float.Parse(line_rot[j]), 0f));
-                    else if (types == 14)
+                    if(types == 14)
                     {
+                        ObjectManager.setSelectedObject(x, z, 14);
                         theTerrain.terrainData =
                            generateTerrain(theTerrain.terrainData, x * 4, z * 4, 4, 4, 0);
                         continue;
                     }
-                    clone.SetActive(true);
-                    ObjectManager.setObject(clone, x, z);
+                    else if (types != 0)
+                    {
+                        ObjectManager.setSelectedObject(x, z, types);
+                        GameObject clone = Instantiate(ObjectManager.getObject(types+""),
+                                        new Vector3(i + 2, 4, j + 2),
+                                        Quaternion.EulerAngles(0f, float.Parse(line_rot[j]), 0f));
+                        clone.SetActive(true);
+                        ObjectManager.setObject(clone, x, z);
+                    }
                 }
             }
-
             data_rot.Close();
             data_type.Close();
+            data_preType.Close();
+
+            File.Delete(preTypePath);
+            File.Delete(typePath);
+            File.Delete(rotPath);
         }
     }
 }
